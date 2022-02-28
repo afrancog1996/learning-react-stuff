dbed3a1e75a6442bb9e31a5f2120a400e7f3a37c
Alejandro Franco Gonzalez <88683803+afrancog1996@users.noreply.github.com>
2022-02-28 17:54:28 -0600

    feat(frontend): added some packages

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,19 @@ import "./helpers/i18n";
 import { lazy, Suspense } from "react";
 
 function App() {
-  const Header = lazy(() => import('./components/header/header.component'));
+  const Header = lazy(() => import("./components/header/header.component"));
+  const Main = lazy(() => import("./components/main/main.component"));
+  const Footer = lazy(() => import("./components/footer/footer.component"));
 
-  return <Suspense fallback={<div>Loading...</div>}>
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
       <Header />
-  </Suspense>
+      <main>
+        <Main />
+      </main>
+      <Footer />
+    </Suspense>
+  );
 }
 
 export default App;
