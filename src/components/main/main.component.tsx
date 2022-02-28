39aa4b39c3522bef9ab80d3d3acb1003179fd6b5
Alejandro Franco Gonzalez <88683803+afrancog1996@users.noreply.github.com>
2022-02-28 17:52:20 -0600

    feat(frontend)=: created main component

--- src/components/main/main.component.tsx
+++ src/components/main/main.component.tsx
@@ -1,12 +1,25 @@
-import { useTranslation } from "react-i18next";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { lazy, Suspense } from "react";
 
 function Main() {
-  const { t } = useTranslation();
+  const Welcome = lazy(() => import("../welcome/welcome.component"));
+  const Instagram = lazy(
+    () => import("../../apps/instagram/instagram.component")
+  );
+  const Spotify = lazy(() => import("../../apps/spotify/spotify.component"));
+  const Supabase = lazy(() => import("../../apps/supabase/supabase.component"));
 
   return (
-    <div>
-
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Welcome />}></Route>
+          <Route path="/spotify-app" element={<Spotify />}></Route>
+          <Route path="/supabase-app" element={<Supabase />}></Route>
+          <Route path="/instagram-clone" element={<Instagram />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </Suspense>
   );
 }
 
