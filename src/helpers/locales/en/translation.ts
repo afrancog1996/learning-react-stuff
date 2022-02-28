52dd109dcfd3b812dd909a7aae4dea701eb5aead
Alejandro Franco Gonzalez <88683803+afrancog1996@users.noreply.github.com>
2022-02-27 21:34:13 -0600

    feat(frontend): added translations files

---
+++ src/helpers/locales/en/translation.ts
@@ -0,0 +1,11 @@
+const translation = {
+    "translation": {
+      "author_name": "Alejandro Franco Gonzalez",
+      "spotify_title": "Spotify api y ReactJs",
+      "supabase_title": "Supabase backend como servicio con ReactJs",
+      "instagram_title": "Instagram clone",
+      "global_title": "Learning ReactJs"
+    }
+  }
+  
+  export default translation;
---
+++ src/helpers/locales/es/translation.ts
@@ -0,0 +1,11 @@
+const translation = {
+  "translation": {
+    "author_name": "Alejandro Franco Gonzalez",
+    "spotify_title": "Spotify Api",
+    "supabase_title": "Backend con supabase",
+    "instagram_title": "Clone de Instagram",
+    "global_title": "Aprendiendo ReactJs"
+  }
+}
+
+export default translation;
