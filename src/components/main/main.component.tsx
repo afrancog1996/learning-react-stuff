5190026655c57d7017944e24c8daea9545c3dc78
Alejandro Franco Gonzalez <88683803+afrancog1996@users.noreply.github.com>
2022-02-28 20:29:24 -0600

    fix(fronend): removed useless imports

--- src/components/main/main.component.tsx
+++ src/components/main/main.component.tsx
@@ -1,5 +1,5 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { lazy, Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
 
 function Main() {
   const Welcome = lazy(() => import("../welcome/welcome.component"));
@@ -10,14 +10,14 @@ function Main() {
   const Supabase = lazy(() => import("../../apps/supabase/supabase.component"));
 
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path="/" element={<Welcome />}></Route>
-          <Route path="/spotify-app" element={<Spotify />}></Route>
-          <Route path="/supabase-app" element={<Supabase />}></Route>
-          <Route path="/instagram-clone" element={<Instagram />}></Route>
-        </Routes>
-    </Suspense>
+    <Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Route path="/" element={<Welcome />}></Route>
+      </Suspense>
+      <Route path="/spotify-app" element={<Spotify />}></Route>
+      <Route path="/supabase-app" element={<Supabase />}></Route>
+      <Route path="/instagram-clone" element={<Instagram />}></Route>
+    </Routes>
   );
 }
 
