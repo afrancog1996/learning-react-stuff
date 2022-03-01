da521090971f365d4b056cebde1554563eb430d3
Alejandro Franco Gonzalez <88683803+afrancog1996@users.noreply.github.com>
2022-02-28 20:33:32 -0600

    fix: change suspence to parent tag

--- src/components/main/main.component.tsx
+++ src/components/main/main.component.tsx
@@ -10,14 +10,14 @@ function Main() {
   const Supabase = lazy(() => import("../../apps/supabase/supabase.component"));
 
   return (
-    <Routes>
-      <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
         <Route path="/" element={<Welcome />}></Route>
-      </Suspense>
-      <Route path="/spotify-app" element={<Spotify />}></Route>
-      <Route path="/supabase-app" element={<Supabase />}></Route>
-      <Route path="/instagram-clone" element={<Instagram />}></Route>
-    </Routes>
+        <Route path="/spotify-app" element={<Spotify />}></Route>
+        <Route path="/supabase-app" element={<Supabase />}></Route>
+        <Route path="/instagram-clone" element={<Instagram />}></Route>
+      </Routes>
+    </Suspense>
   );
 }
 
