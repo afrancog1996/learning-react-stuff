1beb77bdd5343ce177edeb558c27c80381690ab0
Alejandro Franco Gonzalez <88683803+afrancog1996@users.noreply.github.com>
2022-02-28 20:24:03 -0600

    fix(frontend): removed useless imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
+import { HashRouter } from "react-router-dom";
 import "./App.css";
-import "./helpers/i18n";
-import { lazy, Suspense } from "react";
-import { BrowserRouter , HashRouter} from "react-router-dom";
+import Footer from "./components/footer/footer.component";
 import Header from "./components/header/header.component";
 import Main from "./components/main/main.component";
-import Footer from "./components/footer/footer.component";
+import "./helpers/i18n";
 
 function App() {
   return (
