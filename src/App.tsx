a5c530258f11a5e698ac07c461f1b8437deda9a8
Alejandro Franco Gonzalez <88683803+afrancog1996@users.noreply.github.com>
2022-03-06 00:35:08 -0600

    removed instagram and add theme color

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Box } from "@mui/material";
-import Paper from '@mui/material/Paper';
-import { styled } from '@mui/material/styles';
+import Paper from "@mui/material/Paper";
+import { createTheme, styled, ThemeProvider } from "@mui/material/styles";
+import React, { useMemo } from "react";
 import { HashRouter } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/footer/footer.component";
@@ -9,27 +10,41 @@ import Main from "./components/main/main.component";
 import "./helpers/i18n";
 
 const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
   ...theme.typography.body2,
   padding: theme.spacing(1),
-  textAlign: 'center',
+  textAlign: "center",
   color: theme.palette.text.secondary,
 }));
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 function App() {
   return (
     <HashRouter>
-      <Box display="grid" gridTemplateColumns="repeat(12, 1fr)" gap={1}>
-        <Box gridColumn="span 12">
-          <Item><Header /></Item>
+      <ThemeProvider theme={darkTheme}>
+        <Box display="grid" gridTemplateColumns="repeat(12, 1fr)" gap={1}>
+          <Box gridColumn="span 12">
+            <Item>
+              <Header />
+            </Item>
+          </Box>
+          <Box gridColumn="span 12">
+            <Item>
+              <Main />
+            </Item>
+          </Box>
+          <Box gridColumn="span 12">
+            <Item>
+              <Footer />
+            </Item>
+          </Box>
         </Box>
-        <Box gridColumn="span 12">
-          <Item><Main /></Item>
-        </Box>
-        <Box gridColumn="span 12">
-          <Item><Footer /></Item>
-        </Box>
-      </Box>
+      </ThemeProvider>
     </HashRouter>
   );
 }
--- src/components/main/main.component.tsx
+++ src/components/main/main.component.tsx
@@ -3,9 +3,6 @@ import { Route, Routes } from "react-router-dom";
 
 function Main() {
   const Welcome = lazy(() => import("../welcome/welcome.component"));
-  const Instagram = lazy(
-    () => import("../../apps/instagram/instagram.component")
-  );
   const Spotify = lazy(() => import("../../apps/spotify/spotify.component"));
   const Supabase = lazy(() => import("../../apps/supabase/supabase.component"));
   const NotFound = lazy(() => import("../404/notfound.component"));
@@ -14,11 +11,10 @@ function Main() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <Routes>
-        <Route path="/" element={<Supabase />}></Route>
-        <Route path="/welcome" element={<Welcome />}></Route>
+        <Route path="/" element={<Welcome />}></Route>
+        {/* <Route path="/welcome" element={<Welcome />}></Route> */}
         <Route path="/spotify-app" element={<Spotify />}></Route>
         <Route path="/supabase-app" element={<Supabase />}></Route>
-        <Route path="/instagram-clone" element={<Instagram />}></Route>
         <Route path="/in-work" element={<InWork />}></Route>
         <Route path="**" element={<NotFound />}></Route>
       </Routes>
